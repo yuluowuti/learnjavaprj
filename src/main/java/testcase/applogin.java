c4c9876ae0b690d56289c6242a4263e247022c68
yuluowuti <18192496382@163.com>
2018-01-09 17:51:14 +0800

    appium 登录

--- src/main/java/testcase/applogin.java
+++ src/main/java/testcase/applogin.java
@@ -10,20 +10,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+//cmp=com.lq.kldshopping/.activity.SplashActivity
+
 public class applogin {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "");//这句不是必须的
 //        capabilities.setCapability("deviceName","Android Emulator");
-        capabilities.setCapability("deviceName", "yzmd");
+        capabilities.setCapability("deviceName", "ead1cc4a");
         capabilities.setCapability("platformVersion", "4.4");
         capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("appPackage", "com.lianchuan.yzmd");
-        capabilities.setCapability("appActivity", "com.lianchuan.yzmd.activity.SplashActivity");
-        AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        capabilities.setCapability("appPackage", "com.lianchuan.kaledai");
+        capabilities.setCapability("appActivity", "com.lianchuan.kaledai.activity.SplashActivity");
+        AppiumDriver driver = new AppiumDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        WebElement element = driver.findElement(By.id("com.lianchuan.yzmd:id/textView10"));
+        WebElement element = driver.findElement(By.id("com.lianchuan.kaledai:id/repay"));
         System.out.println(element.getText().toString());
+
+        driver.quit();
     }
 }
